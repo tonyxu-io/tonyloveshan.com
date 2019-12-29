5b9e6b25487110ed9104e0ae3c812f9bac669bc7
Tony Xu <yihan.xu@gmail.com>
2019-12-29 15:37:24 -0800

    Use instagram as photo feed

--- src/app/data/i18n/zh.ts
+++ src/app/data/i18n/zh.ts
@@ -31,6 +31,7 @@ export default {
     "san-francisco-city-hall-wedding-ceremony": "旧金山市政厅婚礼仪式",
     "las-vegas-2019": "拉斯维加斯之旅 // 自助餐、赌场、马蹄湾、大峡谷、喷泉表演",
     "portland-2019": "波特兰旅行游记 2019 // 波特兰市区、书店、海边、咖啡吧、餐厅。",
-    "maui-hawaii-2019": "夏威夷茂宜岛蜜月之旅 2019"
+    "maui-hawaii-2019": "夏威夷茂宜岛蜜月之旅 2019",
+    "follow": "关注"
   }
 }
---
+++ src/app/instagram-account.service.ts
@@ -0,0 +1,15 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class InstagramAccountService {
+
+  constructor(public http: HttpClient) {
+  }
+
+  getInstagramAccount(){
+    return this.http.get('https://tonyloveshan-api.azurewebsites.net/api/getMyInstagramAccount?code=LhsQMatMMzqmreNV5ftsThBBEMm2uan5rp7ERqdv3nSfORurAyXJLw==');
+  }
+}
