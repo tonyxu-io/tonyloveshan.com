c64ded43a19e9862b84ed2c5fd32473fc8f7bf29
Tony Xu <yihan.xu@gmail.com>
2020-02-14 16:33:47 -0800

    Add spinner and updated copy

--- src/app/data/i18n/en.ts
+++ src/app/data/i18n/en.ts
@@ -13,26 +13,13 @@ export default {
     "tony-and-helen-vlogs": "Tony and Helen Vlogs",
     "designed-by": "Designed with ❤️ by",
     "subscribe": "SUBSCRIBE",
-    "big-sur-and-pfeiffer-beach": "Big Sur and Pfeiffer Beach",
-    "henry-cowell-state-park": "Henry Cowell State Park",
-    "mountain-view-downtown-dinner-tour": "Mountain View Downtown Dinner Tour",
-    "mountain-view-farmers-market-tour": "Mountain View Farmers Market Tour",
-    "miami-keywest-tour": "Miami and Key West Tour",
     "youtube": "YouTube",
-    "bilibili": "Bilibili",
-    "new-york-city-tour": "New York City Tour and Times Square Proposal",
     "loading": "LOADING...",
     "more-videos": "more videos »",
     "photos": "Photos",
-    "wedding-ceremony": "Wedding Ceremony",
-    "san-francisco-city-hall-california": "San Francisco City Hall, California",
-    "june-18-2019": "June 18, 2019",
     "more-photos": "more photos",
-    "san-francisco-city-hall-wedding-ceremony": "San Francisco City Hall Wedding Ceremony",
-    "las-vegas-2019": "Las Vegas Trip 2019 // Buffet, Casino, Horseshoe Bend, Grand Canyon, Water Show",
-    "portland-2019": "Portland Trip 2019 // Downtown, Bok Bok, Vizcaya Museum & Gardens, Nong's Khao Man Gai, Tea Bar.",
-    "maui-hawaii-2019": "Maui Hawaii Honeymoon Trip at Andaz Maui Resort",
     "follow": "Follow",
-    "loading-from-instagram": "Loading photos from Instagram..."
+    "loading-from-instagram": "Loading photos from Instagram...",
+    "loading-from-youtube": "Loading videos from YouTube..."
   }
 }
